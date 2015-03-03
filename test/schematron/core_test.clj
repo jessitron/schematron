bb48ccfc97f386aed4fd387be7511080a3d99fe1
Jessica Kerr <jessitron@outpace.com>
2015-03-03 18:49:12 +0000

    Refactor test

--- test/schematron/core_test.clj
+++ test/schematron/core_test.clj
@@ -19,21 +19,20 @@
 (defn contains? [string substring]
   (.contains string substring))
 
-(s/defn gimme-the-name-of-this-var [v :- clojure.lang.Var]
+(s/defn name-of [v :- clojure.lang.Var]
   (name (:name (meta v))))
 
 (deftest negative-path
   (defgen failgen s/Str (gen/elements [:a :b :c]))
   (defgen garberator s/Str (gen/elements [:a :b :c]))
-  (doseq [[g n] [[failgen "failgen"] [garberator "garberator"]]]
-    (let [failing-gen g]
-      (if-let [e (is (thrown? clojure.lang.ExceptionInfo
-                              (doall (gen/sample failing-gen))))]
-        (let [ message (.getMessage e)]
-          (is (contains? message "Generated value does not match schema")
-              "Error message should announce cause")
-          (is (contains? message n)
-              "Error message should reveal the name of the generator"))))))
+  (doseq [failing-gen-var [#'failgen #'garberator]]
+    (if-let [e (is (thrown? clojure.lang.ExceptionInfo
+                            (doall (gen/sample @failing-gen-var))))]
+      (let [message (.getMessage e)]
+        (is (contains? message "Generated value does not match schema")
+            "Error message should announce cause")
+        (is (contains? message (name-of failing-gen-var))
+            "Error message should reveal the name of the generator")))))
 
 (deftest name-space-in-error
   (if-let [e (is (thrown? clojure.lang.ExceptionInfo
